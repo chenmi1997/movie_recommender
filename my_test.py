9f64dcdf4ccb9d040c0cb15ec54ceef8fef73893
chenmi1997 <46470453+chenmi1997@users.noreply.github.com>
2019-04-30 18:27:22 -0400

    Update my_test.py
    worked with a friend on this

--- my_test.py
+++ my_test.py
@@ -1,11 +1,14 @@
-# testing-123/my_test.py
-
-from my_script import enlarge # load the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
+from movie_project import random_movie_generator
+import pytest
 
 def test_outputLenIs3():
     output_len = 0
-    for x in range(300):
+    for p in range(300):
         output_len += random_movie_generator("comedy")
     if (output_len == 900):
         print("Assertion Succeeded")
     assert output_len == 900 
+    
+    # WORKED WITH ERIK FOK ON THIS
+
+test_outputLenIs3()
