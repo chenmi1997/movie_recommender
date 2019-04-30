3b661f0abd62cfc1e84d295d1d445d6b05544312
chenmi1997 <46470453+chenmi1997@users.noreply.github.com>
2019-04-30 18:13:53 -0400

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -2,6 +2,10 @@
 
 from my_script import enlarge # load the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
 
-def test_enlarge(): # note the function name is prefixed with "test_"
-    result = len(movie_list) # directly invoke the function we want to test
-    assert result == 300 # describe expectations for desired behavior
+def test_outputLenIs3():
+    output_len = 0
+    for x in range(300):
+        output_len += random_movie_generator("comedy")
+    if (output_len == 900):
+        print("Assertion Succeeded")
+    assert output_len == 900 
